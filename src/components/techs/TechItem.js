665d9314a406592de75097c020fbcda8135e1d69
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2021-03-15 19:54:15 -0600

    Pushing project to github.

---
+++ src/components/techs/TechItem.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { deleteTech } from '../../actions/techActions'
+import M from 'materialize-css/dist/js/materialize.min.js'
+
+const TechItem = ({ deleteTech, tech: { firstName, lastName, id } }) => {
+	const onDelete = () => {
+		deleteTech(id)
+
+		M.toast({ html: `Technician (${firstName} ${lastName}) deleted.` })
+	}
+
+	return (
+		<li className='collection-item'>
+			<div>
+				{firstName} {lastName}
+				<a href='#!' className='secondary-content'>
+					<i className='material-icons grey-text' onClick={onDelete}>
+						delete
+					</i>
+				</a>
+			</div>
+		</li>
+	)
+}
+
+TechItem.propTypes = {
+	tech: PropTypes.object.isRequired,
+	deleteTech: PropTypes.func.isRequired
+}
+
+export default connect(null, { deleteTech })(TechItem)
---
+++ src/components/techs/TechListModal.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import TechItem from './TechItem'
+import { getTechs } from '../../actions/techActions'
+
+const TechListModal = ({ getTechs, tech: { techs, loading } }) => {
+	useEffect(() => {
+		getTechs()
+		// eslint-disable-next-line
+	}, [])
+
+	return (
+		<div id='tech-list-modal' className='modal' style={modalStyle}>
+			<div className='modal-content'>
+				<h4>Technician List</h4>
+				<ul className='collection'>
+					{!loading &&
+						techs !== null &&
+						techs.map(tech => (
+							<li>
+								<TechItem tech={tech} key={tech.id} />
+							</li>
+						))}
+				</ul>
+			</div>
+		</div>
+	)
+}
+
+const modalStyle = {
+	width: '75%',
+	height: '75%'
+}
+
+TechListModal.propTypes = {
+	tech: PropTypes.object.isRequired,
+	getTechs: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+	tech: state.tech
+})
+
+export default connect(mapStateToProps, { getTechs })(TechListModal)
