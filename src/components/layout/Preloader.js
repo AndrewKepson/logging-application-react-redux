c30b0a9c5a7f9bb49647930d38bb0d1e5c9720a3
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2021-05-29 16:16:55 -0600

    Adding repo link to preloader

--- src/components/layout/Preloader.js
+++ src/components/layout/Preloader.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 const Preloader = () => {
 	return (
-		<div className='progress blue lighten-4'>
-			<div className='indeterminate blue'></div>
-		</div>
+		<>
+			<div className='progress blue lighten-4'>
+				<div className='indeterminate blue'></div>
+			</div>
+			<p>
+				Since this application uses a db.json server it must be downloaded and
+				run locally to read and write data:{' '}
+				<a
+					href='https://github.com/AndrewKepson/logging-application-react-redux'
+					rel='noopener noreferrer nofollow'
+					target='_blank'>
+					GitHub Repository
+				</a>
+			</p>
+		</>
 	)
 }
 
